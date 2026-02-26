0cbb5ebb39e9af19e66fb6375bfd0a9982af3a40
Your Name <you@example.com>
2026-02-27 00:42:52 +0700

    fixing

---
+++ Test Suites/KAFKA.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KAFKA</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c366e0d0-877b-4b13-b922-da3af9f7559e</testSuiteGuid>
+   <testCaseLink>
+      <guid>2973f049-dcf6-4f8a-b25c-d865b7314d3d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/kafka/001_TC_ConsumeMessage</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
